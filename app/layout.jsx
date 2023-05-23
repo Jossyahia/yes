fe860058b8a66d91d7afbe2f8d733f74e93ea685
jossyahia <jossyahia@gmail.com>
2023-05-24 00:26:44 +0100

    met

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,6 @@ import Navbar from "@/components/Navbar";
 import SearchBox from "@/components/SearchBox";
 import "./globals.css";
 import Providers from "./Providers";
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
--- app/page.jsx
+++ app/page.jsx
@@ -1,31 +1,27 @@
 // Fix the problem for "search params object is empty in production with next 13 app dir"
 // for more info "https://github.com/vercel/next.js/issues/43077"
 import Results from "./../components/Results";
-export const fetchCache = "auto";
+//export const fetchCache = "auto";
 
-export const dynamic = "force-dynamic"; // this is the fix
-
+//export const dynamic = "force-dynamic"; // this is the fix
 export const metadata = {
   title: "Welcome to Fastfast Delivery Service",
 };
 
+export async function fetchdata() {
+  const res = await fetch(
+    `https://victorious-teal-school-uniform.cyclic.app/api`,
+    { cache: "no-store" }
+  );
+  return res.json()
+}
+
 export default async function Home() {
-  //const likes = searchParams.likes || "fetchTrending";
-  try {
-    const res = await fetch(
-      `https://victorious-teal-school-uniform.cyclic.app/api`,
-      { cache: "no-store" }
-    );
-    const data = await res.json();
-    const results = data;
+const results = await fetchdata()
     return (
       <div>
         <Results results={results} />
       </div>
     );
-  } catch (error) {
-    if (!res.ok) {
-      throw new Error("Failed to fetch data"); // this will be caught by the error page and passed to the page as props
-    }
   }
-}
+
