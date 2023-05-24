9f35895b52109a0ec2e8e4b6e878bb28b72bc8c9
jossyahia <jossyahia@gmail.com>
2023-05-24 01:30:21 +0100

    error hand

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,20 @@
-
 import Results from "./../components/Results";
 export async function fetchdata() {
   const res = await fetch(
     `https://victorious-teal-school-uniform.cyclic.app/api`,
     { cache: "no-store" }
   );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data"); // this will be caught by the error page and passed to the page as props
+  }
   return res.json();
 }
 
 export default async function Home() {
-const results = await fetchdata()
-    return (
-      <div>
-        <Results results={results} />
-      </div>
-    );
-  }
-
+  const results = await fetchdata();
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
+}
