a5c772d842d9da18dda867ed63ec4558a5e3f572
jossyahia <jossyahia@gmail.com>
2023-05-24 03:47:47 +0100

    tr

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,28 @@
+// Fix the problem for "search params object is empty in production with next 13 app dir"
+
+// for more info "https://github.com/vercel/next.js/issues/43077"
+
 export const dynamic = "force-dynamic"; // this is the fix
 
-import Results from "./../components/Results";
-export async function fetchdata() {
+import Results from "@/components/Results";
+
+export default async function Home() {
+ // const likes = searchParams.likes || "fetchTrending";
+
   const res = await fetch(
     `https://victorious-teal-school-uniform.cyclic.app/api`,
-    { cache: "no-store" }
+    { next: { revalidate: 60 } }
   );
+
   if (!res.ok) {
     throw new Error("Failed to fetch data"); // this will be caught by the error page and passed to the page as props
   }
-  return res.json();
-}
 
-export default async function Home() {
-  const results = await fetchdata();
+  const data = await res.json();
+
+  const results = data;
+  //const results.id = results._id;
+
   return (
     <div>
       <Results results={results} />
